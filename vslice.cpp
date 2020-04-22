45cdbd8a4ba6dd99a0bf0f039956c41a2c4ce722
wulc <wulc1994@outlook.com>
2020-04-22 14:50:18 +0800

    2020.4.22

---
+++ vslice.cpp
@@ -0,0 +1,12 @@
+// vslice.cpp -- using valarray slices
+#include <iostream>
+#include <valarray>
+#include <cstdlib>
+
+const int SIZE = 12;
+typedef std::valarray<int> vint;
+void show(const vint & v, int cols);
+int main()
+{
+    
+}
